0677e6acb8ca563bd5646c270c3d264a2afa033b
Tasneemahk <tasneemahk824@gmail.com>
2022-11-02 11:55:59 +0300

    updated website

--- script.js
+++ script.js
@@ -2,24 +2,28 @@ function BMI(weight = 1, height = 1) {
   let result = weight / (height * height);
   return result;
 }
-console.log(BMI());
+console.log(BMI(65, 1.6));
 
-function Status(BodyMass) {
-  if (BodyMass >= 25) {
-    return `Overweight.`;
+function Status(BMI) {
+  if (BMI >= 25) {
+    color = "#FB6107";
+    return ` Overweight.`;
   } else {
-    if (BodyMass >= 18) {
-      return `Healthy weight.`;
+    if (BMI >= 18) {
+      color = "#7DDF64";
+      return ` Healthy weight.`;
     } else {
-      return `Under healthy measures!`;
+      color = "#DE9E36";
+      return `Under healthy measures! `;
     }
   }
 }
 function calculate() {
-  let weight = document.getElementById(weight).value;
-  let height = document.getElementById(height).value;
-  let bmi_value = BMI();
+  let weight = document.getElementById("weightt").value;
+  let height = document.getElementById("heightt").value;
+  let bmi_value = BMI(weight, height);
   let desc = Status(bmi_value);
-  let box = document.getElementById(result);
-  box.innerText = `${bmi_value}== ${desc}`;
+  let box = document.getElementById("result");
+  box.innerText = bmi_value + "==" + desc;
+  box.style.color = color;
 }
